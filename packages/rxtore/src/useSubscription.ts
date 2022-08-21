7e9845581f3b7c1b4f272dbf8ec061f7800a2ffc
nomyfan <Nomyfan@live.com>
2022-08-21 22:49:03 +0800

    refactor: delete useMemorizedFn

--- packages/rxtore/src/useSubscription.ts
+++ packages/rxtore/src/useSubscription.ts
@@ -1,17 +1,15 @@
 import { useEffect } from "react";
 import type { Observable } from "rxjs";
-import { useMemorizedFn } from "./useMemorizedFn";
 
 function useSubscription<T>(
   source$: Observable<T>,
   observer: (value: T) => void
 ) {
-  const memorizedObserver = useMemorizedFn(observer);
   useEffect(() => {
-    const sub = source$.subscribe(memorizedObserver);
+    const sub = source$.subscribe(observer);
 
     return () => sub.unsubscribe();
-  }, [source$, memorizedObserver]);
+  }, [source$]);
 }
 
 export { useSubscription };
